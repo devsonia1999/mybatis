d28ad871378c61aed6d77bc2451a88353e26270a
seonia <heyseonia@gmail.com>
2025-07-10 09:24:43 +0900

    update

--- src/main/java/com/example/demo/controller/RestDeptController.java
+++ src/main/java/com/example/demo/controller/RestDeptController.java
@@ -15,10 +15,18 @@ import java.util.List;
 public class RestDeptController {
     @Autowired
     private DeptService deptService = null;
+
+    @PostMapping
+    public String deptInsert(@RequestBody DeptVO dvo) {
+        int result = 0;
+        result = deptService.deptInsert(dvo);
+        return String.valueOf(result);
+    }
+
     @GetMapping("deptList")
     public String deptList(DeptVO dvo){
         log.info("deptList");
-        log.info(dvo);
+        log.info(dvo);//null, 0,null, null
         List<DeptVO> list = null;
         list = deptService.deptList(dvo);
         Gson gson = new Gson();
@@ -28,7 +36,7 @@ public class RestDeptController {
     @GetMapping("deptDetail")
     public String deptDetail(DeptVO dvo){
         log.info("deptDetail");
-        log.info(dvo);
+        log.info(dvo);//10 or 20 or 30 or 40,  null, null
         List<DeptVO> list = null;
         list = deptService.deptList(dvo);
         Gson gson = new Gson();
@@ -37,20 +45,21 @@ public class RestDeptController {
     }
     @DeleteMapping("deptDelete")
     public String deptDelete(DeptVO dvo){
-        log.info("deptDelete");
-        log.info(dvo);
-        log.info(dvo.getDeptno());
-        int result = 0;
+        log.info("deptDetail");
+        log.info(dvo);//10 or 20 or 30 or 40,  null, null
+        log.info(dvo.getDeptno());//0보다 커야 삭제 처리가 된다.
+        int result=0;
         result = deptService.deptDelete(dvo);
-        return String.valueOf(result);
+        return  String.valueOf(result);
     }
-    //react에서 객체 리터럴로 파라미터를 받아 올 때는 @RequestParam으로는 못 받아옴.
+    //리액트에서 객체 리터럴로 파라미터를 받아 올 때는 @RequestParam으로는
+    //못 받아 온다. 반드시 @RequestBody할 것.
     @PutMapping("deptUpdate")
     public String deptUpdate(@RequestBody DeptVO dvo){
         log.info("deptUpdate");
-        log.info(dvo.getDeptno()+","+dvo.getDname()+","+dvo.getLoc());
-        int result = 0;
+        log.info(dvo.getDeptno()+","+dvo.getDname()+", "+dvo.getLoc());//10 or 20 or 30 or 40,  null, null
+        int result=0;
         result = deptService.deptUpdate(dvo);
-        return String.valueOf(result);
+        return  String.valueOf(result);
     }
 }
